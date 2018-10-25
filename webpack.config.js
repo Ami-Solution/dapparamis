28e21654b0046235298728d0d73d9d1b009d52bd
Glitch (healthy-unicorn) <none>
2018-10-25 14:33:41 +0000

    🌾👦 Updated with Glitch

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var path = require('path');
+//needed to make async await work:
+require('babel-polyfill');
+module.exports = {
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 81920
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  externals: {
+    react: 'commonjs react'
+  }
+};
